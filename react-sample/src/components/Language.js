69436688368eaf7cede651caee7178a6e44494f8
mk1018 <ktmn1018@yahoo.co.jp>
2021-01-26 23:12:49 +0900

    map

--- react-sample/src/components/Language.js
+++ react-sample/src/components/Language.js
@@ -2,13 +2,43 @@ import React from 'react';
 
 class Language extends React.Component {
   render() {
-    return (
-        <div className="language">
+    const languageList = [
+      {
+        name: 'HTML & CSS',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg'
+      },
+      {
+        name: 'JavaScript',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg'
+      },
+      {
+        name: 'React',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg'
+      },
+      {
+        name: 'Ruby',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/ruby.svg'
+      },
+      {
+        name: 'Ruby on Rails',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/rails.svg'
+      },
+      {
+        name: 'Python',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/python.svg'
+      }
+    ];
 
-      <div className='language-item'>
-        <div className='language-name'>HTML & CSS</div>
-        <img className='language-image' src='https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg' />
-      </div>
+    return (
+      <div className="language">
+        {languageList.map((languageItem) => {
+          return (
+            <div className='language-item'>
+                <div className='language-name'>{languageItem.name}</div>
+                <img className='language-image' src={languageItem.image} />
+            </div>
+          )
+        })}
       </div>
     );
   }
