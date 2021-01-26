29cc78c395f04165585bb347c753c4b2d4eaba63
mk1018 <ktmn1018@yahoo.co.jp>
2021-01-26 22:53:27 +0900

    勉強中

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 // import Header from './components/Header'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
@@ -6,25 +7,37 @@ import PageHome from './components/PageHome'
 import PageThread from './components/PageThread'
 import PageNotFound from './components/PageNotFound'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* <h1>タグを用いて、「Hello World」と表示してください */}
+        <h1>Hello World</h1>
+
+        {/* <p>タグを用いて、「一緒にReactを学びましょう！」と表示してください */}
+        <p>一緒にReactを学びましょう！</p>
+        <PageHome />
+      </div>
+    );
+  }
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
--- react-sample/src/components/PageHome.js
+++ react-sample/src/components/PageHome.js
@@ -1,7 +1,36 @@
 import React from 'react'
 
-const PageHome = () => {
-  return <h1>{'Home'}</h1>
-}
+class PageHome extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'ひつじ仙人',
+            count: 0
+        }
+    }
+
+    changeName (name) {
+        this.setState({name: name})
+    }
+
+    countUp() {
+        this.setState({count: this.state.count + 1})
+        console.log(this.state.count)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>こんにちは、{this.state.name}さん！</h1>
+                {/* buttonタグ内に、コンソールに名前を出力するonClickイベントを追加してください */}
+                <button onClick={() => {this.changeName('ひつじ仙人')}}>ひつじ仙人</button>
+
+                {/* buttonタグ内に、コンソールに名前を出力するonClickイベントを追加してください */}
+                <button onClick={() => {this.changeName('にんじゃわんこ')}}>にんじゃわんこ</button>
+                <button onClick={() => {this.countUp()}}>+</button>
+            </div>
+        );
+    }
+}
 export default PageHome
